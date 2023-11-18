20759954fd0f0d2ffcdabbdedc66114a092ba4f9
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-18 23:11:17 +0200

    super_review

--- super-review/super-review/sir-savage-the-21st/js/main.js
+++ super-review/super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,16 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+function print21Sv(num){
+    for (let i = 1; i <= num; i++){
+        console.log('21')
+    }
+}
+print21Sv(21)
+//Bonus can you make it print '21' 21 times to the dom?
+
+function print21Sv(num){
+    for (let i = 1; i <= num; i++){
+        document.querySelector('h2').innerText += ' 21'
+    }
+}
+print21Sv(21)
