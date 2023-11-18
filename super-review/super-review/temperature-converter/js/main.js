600e05be55278e3801e90d2223413869dec381f3
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-18 23:28:43 +0200

    super_review

--- super-review/super-review/temperature-converter/js/main.js
+++ super-review/super-review/temperature-converter/js/main.js
@@ -2,4 +2,11 @@
 //need the value in celcius
 //convert celcius to far
 //show the value
+document.querySelector('button').addEventListener('click', runTemp)
+function runTemp(){
+    let temp = Number(document.querySelector('input').value)
+    let convertor = (temp - 32) * 5/9
+    let p = document.querySelector('p')
+    p.innerText = `${convertor.toFixed(0)}`
+}
 
