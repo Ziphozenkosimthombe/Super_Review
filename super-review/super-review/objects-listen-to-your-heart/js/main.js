c7b4331d838b156fe126600a603a6798417eac3d
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-18 22:42:54 +0200

    super_review

--- super-review/super-review/objects-listen-to-your-heart/js/main.js
+++ super-review/super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {}
+// stopwatch obj properties
+stopwatch.color = 'black'
+stopwatch.brand = 'nike'
+stopwatch.shape = 'round'
+stopwatch.model = '2000001'
+
+//stopwatch obj methods
+stopwatch.start = function(){
+    console.log('runnnnnnnnnnnnnnnnnn')
+}
+stopwatch.stop = function(){
+    console.log('take a breakkkkkkk!!!!!!!!!!!!!!!!!')
+}
+stopwatch.mode = function(){
+    console.log('it about that timeeeeeee')
+}
+
+
