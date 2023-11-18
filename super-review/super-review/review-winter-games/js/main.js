1b05c4ecd838cb4383fed28938cf6a1395388730
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-18 22:48:43 +0200

    super_review

--- super-review/super-review/review-winter-games/js/main.js
+++ super-review/super-review/review-winter-games/js/main.js
@@ -1,1 +1,11 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function arrayOfNum(arr){
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] % 2 === 0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
+}
+console.log(arrayOfNum([1,2,3,4,5,6,7,8,9,10]))
